30fe2d036b25bb41110f7e90a18c364d8509d108
olexandrvlasenko <olexandr.vlasenko@gmail.com>
2019-10-23 14:22:54 +0300

    FIX: minor

--- src/js/modules/parseDataInTable.js
+++ src/js/modules/parseDataInTable.js
@@ -3,11 +3,11 @@ export function parseDataInTable(data) {
   data.map((item) => {
     const HTML = `
       <tr>
-        <td>${(item.name ? item.name : '-')}</td>
-        <td>${(item.contact.email ? item.contact.email : '-')}</td>
-        <td>${(item.contact.phoneNumber ? item.contact.phoneNumber : '-')}</td>
-        <td>${(item.contact.createdAt ? item.contact.createdAt : '-')}</td>
-        <td>${(item.banExpiryDate ? item.banExpiryDate : '-')}</td>
+        <td>${(item.name ? item.name : '—')}</td>
+        <td>${(item.contact.email ? item.contact.email : '—')}</td>
+        <td>${(item.contact.phoneNumber ? item.contact.phoneNumber : '—')}</td>
+        <td>${(item.contact.createdAt ? item.contact.createdAt : '—')}</td>
+        <td>${(item.banExpiryDate ? item.banExpiryDate : '—')}</td>
         <td class="${(item.active ? 'uk-text-success' : 'uk-text-danger')}">${(item.active ? 'Active' : 'Not active')}</td>
       </tr>
     `;
